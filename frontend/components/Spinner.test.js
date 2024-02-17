197a7d442bd7d35e452dcfd0f227b5168552cc7a
Robert Dixon <robmsc24@gmail.com>
2024-02-16 20:57:40 -0500

    Update changes

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,12 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // Importing the matcher
+import Spinner from './Spinner';
+
+test('Spinner renders when "on" prop is true', () => {
+  const { getByText } = render(<Spinner on={true} />);
+  const spinnerElement = getByText('Please wait...');
+  expect(spinnerElement).toBeInTheDocument();
+});
